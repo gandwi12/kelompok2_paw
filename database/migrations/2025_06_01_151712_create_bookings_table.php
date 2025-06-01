4eb7e7e20c819bfabc7574262e591ce36915c85b
Muhammad Aydin <muhaydin0405@gmail.com>
2025-06-02 00:18:12 +0700

    menambah migrasi tabel mahasiswa

--- database/migrations/2025_06_01_151712_create_bookings_table.php
+++ database/migrations/2025_06_01_151712_create_bookings_table.php
@@ -1,26 +1,24 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();  
+            $table->foreignId('doctor_id')->constrained('jadwals')->onDelete('cascade');
+            $table->string('nama');  
+            $table->string('nim');   
+            $table->string('diagnosa');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('bookings');
     }
