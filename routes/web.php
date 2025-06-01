df4beb12a972da2a508f572a3256436ff1739e02
Muhammad Aydin <muhaydin0405@gmail.com>
2025-06-02 02:17:52 +0700

    mengatur route web nya

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\PemeriksaanRiwayatController;
 
 
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -39,6 +40,12 @@ Route::get('/dashboard', function () {
 
 
     /*  Routes untuk Fitur Pemeriksaan  */
+    Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');          // Tampilkan daftar bookings
+Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create');  // Form tambah booking
+Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');          // Simpan booking baru
+Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit'])->name('bookings.edit'); // Form edit booking
+Route::put('/bookings/{booking}', [BookingController::class, 'update'])->name('bookings.update');  // Simpan perubahan
+Route::delete('/bookings/{booking}', [BookingController::class, 'destroy'])->name('bookings.destroy'); // Hapus booking
 
 
     /*  Routes untuk Fitur Users  */
