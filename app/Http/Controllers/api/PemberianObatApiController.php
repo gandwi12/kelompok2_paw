20c09c341a0dad603e83d3c40d8567954c4c3682
Krisna2004 <kadekkrisnandikaa@gmail.com>
2025-06-02 10:19:00 +0700

    add pemberian obat controller

--- app/Http/Controllers/api/PemberianObatApiController.php
+++ app/Http/Controllers/api/PemberianObatApiController.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PemberianObat;
 use Illuminate\Http\Request;
 
 class PemberianObatApiController extends Controller
 {
-    //
+    public function index()
+    {
+        // Ambil semua data pemberian obat beserta relasi obat dan user
+        $pemberians = PemberianObat::with(['obat', 'user'])->get();
+
+        // Kembalikan dalam format JSON
+        return response()->json($pemberians);
+    }
 }
